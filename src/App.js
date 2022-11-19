2768a58b86cb5b71dcb80300db1c2809d2e702ab
Michał Wójcik <01153102@pw.edu.pl>
2022-11-20 00:35:39 +0100

    Finished web app

--- src/App.js
+++ src/App.js
@@ -1,57 +1,94 @@
-import { Button, Typography } from '@mui/material';
-import Container from '@mui/material/Container';
-import { Box, Stack } from '@mui/system';
-import { useTheme } from '@mui/material/styles';
-import { grey } from '@mui/material/colors';
-import { useState } from 'react';
-import { PREDICTION_KEY, PREDICTION_API_URL } from './constants'
+import { Button, Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import { Box, Stack } from "@mui/system";
+import { grey } from "@mui/material/colors";
+import { useState } from "react";
+import { PREDICTION_KEY, PREDICTION_API_URL, PREDICTION_MAKEUP, PREDICTION_NAGATIVE, PREDICTION_NO_MAKEUP } from "./constants";
+
 
 function App() {
-
   const [uploadedImage, setUploadedImage] = useState(null);
   const [predictionResultMessage, setPredictionResultMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const toBase64 = file => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
+  const handlePredictionsResult = (predictionsArray) => {
+
+    console.log(predictionsArray)
+
+    const maxPropabilityPredictionObj = predictionsArray.reduce((prevPrediction, currentPrediction) => {
+      return (prevPrediction.probability > currentPrediction.probability) ? prevPrediction : currentPrediction
+    });
+
+    const percentageProbability = (maxPropabilityPredictionObj.probability * 100).toFixed(2);
+    let predictionMessage;
+
+    if (maxPropabilityPredictionObj.tagName === PREDICTION_MAKEUP) {
+      predictionMessage = "It is likely that you're wearing makeup with " + percentageProbability + "% probability";
+    } else if (maxPropabilityPredictionObj.tagName === PREDICTION_NO_MAKEUP) {
+      predictionMessage = "It is likely that you're not wearing makeup with " + percentageProbability + "% probability";
+    } else if (maxPropabilityPredictionObj.tagName === PREDICTION_NAGATIVE) {
+      predictionMessage = "The photo you uploaded is most likely not human face with " + percentageProbability + "% probability";
+    }
+
+    setPredictionResultMessage(predictionMessage);
+  }
 
   const handleFileUpload = async (e) => {
     const imageFile = e.target.files[0];
     if (imageFile) {
+
+      setErrorMessage("");
+      setPredictionResultMessage("");
       setUploadedImage(imageFile);
-      const response = await fetch(PREDICTION_API_URL, {
-        method: "POST",
-        headers: {
-          "Prediction-Key": PREDICTION_KEY,
-          "Content-Type": 'application/octet-stream'
-        },
-        body: toBase64(imageFile)
-      }).catch((error) => {
-        console.log(error);
-      });
-      console.log(response);
-      setPredictionResultMessage(response);
+
+      try {
+        const response = await fetch(PREDICTION_API_URL, {
+          method: "POST",
+          headers: {
+            "Prediction-Key": PREDICTION_KEY,
+            "Content-Type": "application/octet-stream",
+          },
+          body: new Blob([imageFile]),
+        });
+
+        if (!response.ok) {
+          const errorBody = await response.json();
+          const errorMessage = await errorBody.message;
+          throw new Error(errorMessage);
+        }
+
+        const responseJson = await response.json();
+        handlePredictionsResult(responseJson.predictions);
+
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
     }
-  }
+  };
 
   return (
-    <Container sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      paddingTop: '24px',
-      rowGap: '30px',
-      alignItems: 'center'
-    }}>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: "24px",
+        rowGap: "30px",
+        alignItems: "center",
+      }}
+    >
       <Stack textAlign="center" rowGap="15px">
         <Typography variant="h2">Makeup detector</Typography>
-        <Typography fontSize="18px">Upload a photo of your face and verify if you're wearing a makeup</Typography>
+        <Typography fontSize="18px">
+          Upload a photo of your face and verify if you're wearing a makeup
+        </Typography>
       </Stack>
-      <Button variant="contained" component="label" sx={{
-        width: '300px'
-      }}>
+      <Button
+        variant="contained"
+        component="label"
+        sx={{
+          width: "300px",
+        }}
+      >
         Upload Photo
         <input
           type="file"
@@ -61,19 +98,25 @@ function App() {
         />
       </Button>
 
-      {!uploadedImage &&<Container sx={{
-        width: '650px',
-        height: '550px',
-        backgroundColor: grey[300],
-        borderRadius: 1
-      }} />}
-      
-      {uploadedImage && 
-      <Box height={550} sx={{ borderRadius: 1, overflow: 'hidden'}}>
-        <img src={URL.createObjectURL(uploadedImage)} height={550} />
-      </Box>}
+      {!uploadedImage && (
+        <Container
+          sx={{
+            width: "650px",
+            height: "550px",
+            backgroundColor: grey[300],
+            borderRadius: 1,
+          }}
+        />
+      )}
 
-      <Typography fontSize="18px">{predictionResultMessage}</Typography>
+      {uploadedImage && (
+        <Box height={550} sx={{ borderRadius: 1, overflow: "hidden" }}>
+          <img src={URL.createObjectURL(uploadedImage)} height={550} />
+        </Box>
+      )}
+
+      {predictionResultMessage && <Typography fontSize="18px">{predictionResultMessage}</Typography>}
+      <Typography fontSize="18px" color='error.main'>{errorMessage}</Typography>
     </Container>
   );
 }
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
 export const PREDICTION_KEY = "c772232852e84865a4b9d5aff84385b1";
 export const PREDICTION_API_URL = "https://makeupdetectorcv-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/1824e70d-060b-4fd3-8e0c-ca385247b6e4/classify/iterations/makeup-detector-model/image";
+export const PREDICTION_MAKEUP = 'makeup';
+export const PREDICTION_NO_MAKEUP = 'no-makeup';
+export const PREDICTION_NAGATIVE = 'Negative';
